2355199a7a0498dcd21993e50c440adabb33dd36
Dan-glit <danielgomezo@ielacandelariamedellin.edu.co>
2026-03-24 21:09:39 +0000

    oña

---
+++ 24-03/ejercicio1-1.js
@@ -0,0 +1,9 @@
+var nombre = "Yulian"
+let edad = 16
+const ciudad = "Medellin"
+
+console.log (nombre) ;
+console.log (edad) ;
+console.log (ciudad) ;
+
+console.log("nombre:",nombre,"edad:",edad,"ciudad:",ciudad)
---
+++ 24-03/ejercicio1-5.js
@@ -0,0 +1,7 @@
+{
+let x = 5;
+}
+let y =10;
+console.log(x);
+console.log(y);
+// no funciona por que x esta definida dentro de un bloque y no es accesible fuera de el//  
